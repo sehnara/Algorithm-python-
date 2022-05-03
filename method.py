72b343ece8b033543e366de6ca02a358e842ea7e
sehnara <gsh723@naver.com>
2022-05-03 13:15:57 +0900

    [메서드] 소수 판별 공식 추가

--- method.py
+++ method.py
@@ -1,14 +1,18 @@
-# 1. 소수 판별(에라토스테네스의 체)
+# 1. 소수 판별(에라토스테네스의 체) > True or False
 def is_prime(n) :
-    arr = [True] * (n+1);
-    arr[0] = False;
-    arr[1] = False;
-    
-    for i in range(2,n+1):
-        if arr[i] == True:
-            j = 2;
-            
-            while (i*j) <= n:
-                arr[i*j] = False;
-                j += 1;
-    return arr;
+    prime = [False,False]+ [True] * n;
+    for i in range(2, int((n+1)**0.5)+1):
+        if prime[i] :
+            for j in range(2*i, n+1, i):
+                prime[j] = False;
+    return prime;
+
+# 2. n까지의 소수 목록 산출 
+def get_prime(n):
+    prime = [False,False]+ [True] * n;
+    for i in range(2, int((n+1)**0.5)+1):
+        if prime[i] :
+            for j in range(2*i, n+1, i):
+                prime[j] = False;
+                
+    return [k for k in range(2,n) if prime[k] == True];
